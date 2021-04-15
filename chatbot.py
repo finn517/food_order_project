42a808946c023bf876d0f1399a202c7d68673ea3
finn517 <68793203+finn517@users.noreply.github.com>
2021-04-15 17:07:34 -0400

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 from speech import speak
 from chat import process
 from speech_to_text import get_speech
-import Order as o
+import order as o
 
 # import whatever zubair's thng is
 # initialize Jacob
@@ -9,14 +9,17 @@ import Order as o
 menu = o.initMenu()
 order = []
 
-speak("Welcome, what would you like to order")
+speak("Welcome, what would you like to order?")
 while (True):
-    sentance = str(get_speech())
-    if (sentance == "no" or sentance == "No" or sentance == "quit"):
-        speak(o.printOrder(order))
-        break
-    item = process(sentance)
-    o.addItem(order, menu, item)
-    s = "One" + item
-    speak(s)
-    speak("Is there anything else you would like to order?")
+	sentance = str(get_speech())
+	item = process(sentance)
+	if (item == "quit"):
+		speak(o.printOrder(order))
+		speak("Thank you for ordering!, Have a nice day!")
+		break
+	o.addItem(order, menu, item)
+	s = "One" + item
+	speak(s)
+	speak("Is there anything else you would like to order?")
+
+
